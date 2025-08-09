ba0ce0a72adf87a7bb06e7c96621acb32e501dfd
mohamed <mohamed.mostafa.ibrahim.2005@gmail.com>
2025-08-10 02:37:34 +0300

    fix component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import NavBar from "./components/Navbar"
+import NavBar from "./components/NavBar"
 import Contact from "./sections/Contact"
 import ExperienceSection from "./sections/ExperienceSection"
 import FeatureCards from "./sections/FeatureCards"
